42a8e1e64d426d56ce50f105b0bdb1142d2e9ccf
bagel786 <safbaig5@gmail.com>
2026-04-26 00:58:33 -0500

    reverted tween

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -7105,14 +7105,13 @@ export default class GameScene extends Phaser.Scene {
         }
 
         const travelDistance = Math.abs(MACHINE_PRESENTATION.conveyorExitX - this._unitContainer.x);
-        const exitDurationMs = Math.max(200, Math.round((travelDistance / MACHINE_PRESENTATION.conveyorSpeedPxPerSecond) * 1000));
         const conveyorLayers = [this._mainViewLayers?.mainview_bottom].filter(Boolean);
         if (conveyorLayers.length > 0) {
             this._conveyorAnimTween?.stop();
             this._conveyorAnimTween = this.tweens.add({
                 targets: conveyorLayers,
                 tilePositionX: `+=${travelDistance}`,
-                duration: exitDurationMs,
+                duration: 500,
                 ease: 'Linear',
             });
         }
@@ -7120,7 +7119,7 @@ export default class GameScene extends Phaser.Scene {
         this._unitExitTween = this.tweens.add({
             targets: this._unitContainer,
             x: MACHINE_PRESENTATION.conveyorExitX,
-            duration: exitDurationMs,
+            duration: 500,
             ease: 'Linear',
             onComplete: () => {
                 this._unitExitTween = null;
